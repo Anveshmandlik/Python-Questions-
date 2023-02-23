bb574fde06e9a6de6727b2eb8363860724d6c897
Anveshmandlik <anveshmandlik@gmail.com>
2023-02-23 23:58:03 +0530

    Create greatest number.py

---
+++ greatest number.py
@@ -0,0 +1,12 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+if a>b:
+    if a>c:
+        print("a is greatest.")
+elif b>c:
+    if b>a:
+        print("b is greatest.")
+else:
+    print("c is the greatest.")
